18a139ac1a0f4ce8a52cab33ddd42a8a827a4ab1
daelsaid <daelsaid@stanford.edu>
2018-10-03 04:30:51 -0700

    template script path added to participants tsv function

--- bids_templates/participants_tsv_final_template.sh
+++ bids_templates/participants_tsv_final_template.sh
@@ -2,8 +2,9 @@
 
 function gen_participant_csv_and_convert_to_tsv(){
 	file_to_convert=$1;
-	python /Users/daelsaid/scripts/bids/templates/create_participant_csv_intermediate.py >> participants.csv;
+	template_scripts_path=$2;
+	python ${template_scripts_path}/create_participant_csv_intermediate.py >> participants.csv;
 	new_tsv_filename=`echo ${file_to_convert} | cut -d'.' -f1`;
 	cat ${file_to_convert} | tr "," "\\t" >> ${new_tsv_filename}.tsv
 }
-gen_participant_csv_and_convert_to_tsv
+gen_participant_csv_and_convert_to_tsv #file #template path
